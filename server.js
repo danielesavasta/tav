f210b41e02fd36ad446d79f5bcb056b13a70020e
Daniele Savasta <daniele.savasta@gmail.com>
2024-08-15 17:39:05 +0300

    .

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ const PORT =  10000;
 const host = ("RENDER" in process.env) ? `0.0.0.0` : `localhost`;
 const assets = 'https://taviloglukoleksiyon.org/eserler';
 
+const dbWorks ='works';
+const dbArtists='artist';
+
 const server = Fastify({
   logger: true
 });
@@ -26,46 +29,48 @@ server.register(fastifyView, {
   viewExt: "hbs", // Sets the default extension to `.handlebars`
   includeViewExtension: true,
 });
+
 var hbs = handlebars.create({});
+
 /*
 server.register(fastifyWs, {
   clientTracking: true // enable client tracking
 });*/
-server.register(dbConnector);
 
+server.register(dbConnector);
 server.get("/", async function (req, reply) {
   try {
-    const collection = server.mongo.db.collection('work')
-    const result = await collection.find().project({id:1,title:1,artist_id:1}).toArray()
-    return reply.view("views/wall/index.hbs", { title: "grouper", works:JSON.stringify(result), assets:assets }, {layout: "views/templates/layout.hbs"});
+    const collection = server.mongo.db.collection(dbWorks)
+    const result = await collection.aggregate([{ $sample: { size: 1 } }]).toArray()
+    const artistCollection = server.mongo.db.collection(dbArtists)
+    const artistResult = await artistCollection.findOne({'id':result[0].artist_id})
+    
+    return reply.view("views/wall/home.hbs", { title: "grouper", work:JSON.stringify(result), artist:JSON.stringify(artistResult), assets:assets }, {layout: "views/templates/layout.hbs"});
   } catch (error) {
     console.log(error);
     return "Error Found";
   }
 });
-
 server.get("/gallery/:group", async function (req, reply) {
   try {
-    const collection = server.mongo.db.collection('work')
+    const collection = server.mongo.db.collection(dbWorks)
     const result = await collection.find().toArray()
-    const artistCollection = server.mongo.db.collection('artist')
+    const artistCollection = server.mongo.db.collection(dbArtists)
     const artistResult = await artistCollection.find().toArray()
     const { group } = req.params;
     console.log(group);
     const grouplist = await collection.distinct(group);
-
     return reply.view("views/wall/index.hbs", { title: "grouper", works:JSON.stringify(result), assets:assets, groups:"",artists:JSON.stringify(artistResult), groupsfieldname: group, groups:JSON.stringify(grouplist) }, {layout: "views/templates/layout.hbs"});
   } catch (error) {
     console.log(error);
     return "Error Found";
   }
 });
-
 server.get("/gallery", async function (req, reply) {
   try {
-    const collection = server.mongo.db.collection('work')
+    const collection = server.mongo.db.collection(dbWorks)
     const result = await collection.find().toArray()
-    const artistCollection = server.mongo.db.collection('artist')
+    const artistCollection = server.mongo.db.collection(dbArtists)
     const artistResult = await artistCollection.find().toArray()
    return reply.view("views/wall/index.hbs", { title: "grouper", works:JSON.stringify(result), assets:assets, groups:"",artists:JSON.stringify(artistResult), groups:JSON.stringify("[]") }, {layout: "views/templates/layout.hbs"});
    
@@ -74,27 +79,24 @@ server.get("/gallery", async function (req, reply) {
     return "Error Found";
   }
 });
-
 server.get("/groups/:group", async function (req, reply) {
   try {
-    const collection = server.mongo.db.collection('work')
+    const collection = server.mongo.db.collection(dbWorks)
     const result = await collection.find().toArray()
-    const artistCollection = server.mongo.db.collection('artist')
+    const artistCollection = server.mongo.db.collection(dbArtists)
     const artistResult = await artistCollection.find().toArray()
     const { group } = req.params;
     console.log(group);
     const grouplist = await collection.distinct(group);
-
     return reply.view("views/wall/groups.hbs", { title: "grouper", works:JSON.stringify(result), assets:assets,groups:"",artists:JSON.stringify(artistResult), groupsfieldname: group, groups:JSON.stringify(grouplist) }, {layout: "views/templates/layout.hbs"});
   } catch (error) {
     console.log(error);
     return "Error Found";
   }
 });
-
 server.get("/timeline", async function (req, reply) {
   try {
-    const collection = server.mongo.db.collection('work')
+    const collection = server.mongo.db.collection(dbWorks)
     const result = await collection.find().project({id:1,title:1,date:1}).toArray()
     return reply.view("views/wall/timeline.hbs", { title: "grouper", works:JSON.stringify(result), assets:assets}, {layout: "views/templates/layout.hbs"});
   } catch (error) {
@@ -102,17 +104,15 @@ server.get("/timeline", async function (req, reply) {
     return "Error Found";
   }
 });
-
 server.get("/about", async function (req, reply) {
   return reply.view("views/wall/about.hbs", { title: "about"}, {layout: "views/templates/layout.hbs"});
   });
-/*
+  /*
 server.get("/controller", async function (req, reply) {
   try {
-    const collection = server.mongo.db.collection('work')
-    const artistCollection = server.mongo.db.collection('artist')
+    const collection = server.mongo.db.collection(dbWorks)
+    const artistCollection = server.mongo.db.collection(dbArtists)
     const artistResult = await artistCollection.find().toArray()
-
     var groups = await collection.distinct("technique");
     let imagesByGroups = [];
     let j = 0,
@@ -126,14 +126,12 @@ server.get("/controller", async function (req, reply) {
        j++;
      }
     const techniquesResult=imagesByGroups;
-
     return reply.view("views/mobile/index.hbs", { title: "grouper", artists:JSON.stringify(artistResult), assets:assets, techniques:JSON.stringify(techniquesResult) },{layout:"views/templates/mobile.hbs"}); //{layout:"views/templates/mobile.hbs"},
   } catch (error) {
     console.log(error);
     return "Error Found";
   }
 });
-
 server.register(async function (server) {
 server.get('/comm', { websocket: true }, (connection, req) => {
   // New user
@@ -157,17 +155,15 @@ server.get('/comm', { websocket: true }, (connection, req) => {
       });
   });
 });
-});
-*/
+});*/
 server.ready().then(() => {
   server.listen({ port: PORT, host:host}, (err) => {
     if (err) throw err;
     console.log(`server listening on ${server.server.address().port}`);
   });
 });
-/*
 function broadcast(message) {
   for(let client of server.websocketServer.clients) {
       client.send(JSON.stringify(message));
   }
-}*/
+}
